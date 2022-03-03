3b34c256f6fe669e90ed2010faf60fc6da2f22c8
Hinux Chau <44890821+HinWaei@users.noreply.github.com>
2022-03-03 12:52:01 +0800

    generator v1.0

--- clientGen.sh
+++ clientGen.sh
@@ -2,10 +2,12 @@ wg genkey | tee privKeyClient
 cat privKeyClient | wg pubkey | tee pubKeyClient
 privKeyClient=$(cat privKeyClient)
 pubKeyClient=$(cat pubKeyClient)
-read -p "Please enter the public key of your server:" -e srvPubKey
+
+read -p "Please enter the public key of your server:" -i $(cat pubkey) -e srvPubKey
 read -p "Please enter the domain/IP and port of your server[e.g. example.com:21111]:" -e endpoint
 read -p "Please enter the range of allowed ips[0.0.0.0/0]:" -i "0.0.0.0/0" -e allowedips
-read -p "Please enter the internal address in the LAN [e.g. 192.0.0.1/24]" -e clientInternalIP
+echo "There has been $(cat mygame.conf | grep -Eo "\[Peer\]" | wc -l) peers existing"
+read -p "Please enter the internal address in the LAN [e.g. 192.0.0.1/32]" -e clientInternalIP
 read -p "Please enter DNS[1.1.1.1]:" -i "1.1.1.1" -e DNS
 read -p "Please enter MTU[1420]" -i "1420" -e MTU
 read -p "Please enter the private key of your client["+$privKeyClient+"]:" -i $privKeyClient -e clientPrivKey
